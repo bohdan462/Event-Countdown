04f860e8cb2d69493b5fa2e24d668ae15cd385ed
Bohdan Tkachenko <techbohdan@gmail.com>
2024-06-24 23:14:01 -0500

    Initial Commit

---
+++ Event Countdown/Event_CountdownApp.swift
@@ -0,0 +1,17 @@
+//
+//  Event_CountdownApp.swift
+//  Event Countdown
+//
+//  Created by Bohdan Tkachenko on 6/24/24.
+//
+
+import SwiftUI
+
+@main
+struct Event_CountdownApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
